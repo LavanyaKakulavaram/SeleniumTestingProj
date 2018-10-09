3cb6e9ef080c7728c8df29c3a63df809434aeda3
LavanyaKakulavaram <43801793+LavanyaKakulavaram@users.noreply.github.com>
2018-10-08 23:26:40 -0500

    Update locator.java

--- Selenium/src/selpak/locator.java
+++ Selenium/src/selpak/locator.java
@@ -10,6 +10,7 @@ public class locator {
 	
 		WebDriver driver= new FirefoxDriver();
 		 driver.get("http://facebook.com");
+		system.out.println("Facebook brower is selected");
 		 driver.findElement(By.xpath("//input[@id='email']")).sendKeys("lavanya2055@gmail.com");
 		 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("chittitalli");
 		 driver.findElement(By.xpath("//input[@id='u_0_n']")).click();
